e2d9f1f9c18ae72ff067d95459d98d4006e1d4fe
Gustav Karlsson <gustav.karlsson@gmail.com>
2011-12-04 22:28:38 +0100

    Deleted all GUI.

--- snap/src/main/java/se/gustavkarlsson/snap/main/Snap.java
+++ snap/src/main/java/se/gustavkarlsson/snap/main/Snap.java
@@ -2,22 +2,15 @@ package se.gustavkarlsson.snap.main;
 
 import org.apache.log4j.Logger;
 
-import se.gustavkarlsson.snap.gui.SnapWizard;
-import se.gustavkarlsson.snap.gui.StartPage;
-import se.gustavkarlsson.snap.gui.Wizard;
 import se.gustavkarlsson.snap.util.LoggerHelper;
 
 public class Snap {
 
 	private static final Logger log = LoggerHelper.getLogger();
-	public static String APP_NAME = "Snap";
-	public static String VERSION = "2.0.0-SNAPSHOT";
+	public static final String APP_NAME = "Snap";
+	public static final String VERSION = "2.0.0-SNAPSHOT";
 
 	public static void main(String[] args) {
-		Wizard wiz = new SnapWizard();
-		wiz.setTitle(APP_NAME + " " + VERSION);
-		wiz.setPage(new StartPage());
-		wiz.show();
 	}
 }
 
